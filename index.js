da999f0e32e4bad567238e3a7be3f979b81d647c
Manzi Elyse <manzi.elyse@teamflow.de>
2024-01-23 20:26:09 +0200

    display messages and test socket

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { Server } = require('socket.io');
+const { join } = require('node:path');
 
 // Services
 const AuthService = require('./services/auth.services');
@@ -19,18 +20,23 @@ let messageService = new MessageService()
 // variables
 const port = 3000
 
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
 app.get('/', async (req, res) => {
+  // console.log("returning index.html")
+  // res.sendFile(__dirname + '/index.html');
+  res.sendFile(join(__dirname, 'index.html'));
+  // initialize db and tables
 
-  const sequelize = new Db().getConnection();
-  sequelize.define('User', UserModel);
+  // const sequelize = new Db().getConnection();
+  // sequelize.define('User', UserModel);
 
-  sequelize.sync().then(() => {
-    res.send('Tables created successfully');
-  }).catch((error) => {
-    res.send('Unable to create tables : ' + error);
-  });
+  // sequelize.sync().then(() => {
+  //   res.send('Tables created successfully');
+  // }).catch((error) => {
+  //   res.send('Unable to create tables : ' + error);
+  // });
+  // res.send("Hello World")
 
 })
 
@@ -77,6 +83,20 @@ app.post('/chat', authenticateToken, async (req, res) => {
     "code": 200,
   })
 })
+
+app.get('/test', (req, res) => {
+  io.emit('test', { message: 'hello' });
+  res.send({
+    "message": "success", 
+    "code": 200,
+  })
+})
+
+io.on('connection', (socket) => {
+  socket.on('test', (msg) => {
+    console.log("test event received");
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
