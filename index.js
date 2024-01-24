32acbf71b41945dc4055c3efa5817d0e5b52a545
Manzi Elyse <manzi.elyse@teamflow.de>
2024-01-24 09:04:17 +0200

    socket works now

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { Server } = require('socket.io');
+const socket = require('socket.io');
 const { join } = require('node:path');
 
 // Services
@@ -9,23 +9,25 @@ const { authenticateToken } = require('./helpers/middleware');
 const { Db } = require('./helpers/db');
 const { UserModel } = require('./models/User.model');
 
+// variables
+const port = 3000
+
 // Initializations
 const app = express()
 app.use(express.json())
-const server = require('http').createServer(app);
-const io = new Server(server);
+const server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// const server = require('http').createServer(app);
+const io = socket(server);
 let messageService = new MessageService()
 
-
-// variables
-const port = 3000
 
 // app.use(express.static(__dirname));
 
 app.get('/', async (req, res) => {
-  // console.log("returning index.html")
-  // res.sendFile(__dirname + '/index.html');
   res.sendFile(join(__dirname, 'index.html'));
+  
   // initialize db and tables
 
   // const sequelize = new Db().getConnection();
@@ -36,7 +38,6 @@ app.get('/', async (req, res) => {
   // }).catch((error) => {
   //   res.send('Unable to create tables : ' + error);
   // });
-  // res.send("Hello World")
 
 })
 
@@ -93,11 +94,12 @@ app.get('/test', (req, res) => {
 })
 
 io.on('connection', (socket) => {
+  console.log('a user connected');
   socket.on('test', (msg) => {
     console.log("test event received");
   })
+  socket.on("disconnect", () => {
+    console.log("a user disconnected")
+    io.emit("a user disconnected", socket.userId);
+  });
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
